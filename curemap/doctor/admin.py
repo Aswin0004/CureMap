ba232a3f2f1268676b28ca5592df53d3a1a77ffb
Aswin0004 <aswinsudhan0004@gmail.com>
2025-06-06 15:08:39 +0530

    doctor&hospital_commit

--- curemap/doctor/admin.py
+++ curemap/doctor/admin.py
@@ -1,22 +1,28 @@
 from django.contrib import admin
-from .models import Doctor,DoctorConsultationTimes,DoctorBlog
-# Register your models here.
+from .models import Doctor, DoctorConsultationTimes, DoctorBlog, DoctorVideo
 
 @admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = (
-        'first_name', 'second_name', 'last_name', 'role', 'hospital',
-        'specialization_display', 'experience_years', 'is_available', 'rating'
-    )
-    list_filter = ('hospital', 'specialization', 'available_days', 'is_available')
-    search_fields = ('first_name', 'second_name', 'last_name', 'email', 'hospital__name')
-    readonly_fields = ('rating',)
+    list_display = ('first_name', 'last_name', 'hospital', 'get_specialization', 'is_available')
+    search_fields = ('first_name', 'last_name', 'hospital__hospital_name')
+    list_filter = ('hospital', 'specialization', 'is_available')
 
-    def specialization_display(self, obj):
-        return ", ".join(obj.specialization)
-    specialization_display.short_description = 'Specializations'
+    def get_specialization(self, obj):
+        return ", ".join(obj.get_specialization_display())
+    get_specialization.short_description = 'Specializations'
 
+@admin.register(DoctorConsultationTimes)
+class DoctorConsultationTimesAdmin(admin.ModelAdmin):
+    list_display = ('doctor',)
 
+@admin.register(DoctorBlog)
+class DoctorBlogAdmin(admin.ModelAdmin):
+    list_display = ('doctor', 'title', 'is_published', 'created_at')
+    search_fields = ('doctor__first_name', 'doctor__last_name', 'title')
+    list_filter = ('is_published', 'created_at')
 
-admin.site.register(DoctorBlog)
-admin.site.register(DoctorConsultationTimes)
+@admin.register(DoctorVideo)
+class DoctorVideoAdmin(admin.ModelAdmin):
+    list_display = ('doctor', 'title', 'is_published', 'created_at')
+    search_fields = ('doctor__first_name', 'doctor__last_name', 'title')
+    list_filter = ('is_published', 'created_at')
